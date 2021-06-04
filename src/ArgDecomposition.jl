506bdc5898e1d895d2b61362376d9911d3eecfc0
chriselrod <elrodc@gmail.com>
2021-06-04 03:26:40 -0400

    Create repo

--- src/ArgDecomposition.jl
+++ src/ArgDecomposition.jl
@@ -1,5 +1,70 @@
 module ArgDecomposition
 
-# Write your package code here.
+export flatten_to_tuple, reassemble_tuple
+
+struct StaticType{T} end
+gettype(::StaticType{T}) where {T} = T
+
+function _append_fields!(t::Expr, body::Expr, sym::Symbol, ::Type{T}) where {T}
+  for f ∈ 1:fieldcount(T)
+    TF = fieldtype(T, f)
+    Base.issingletontype(TF) && continue
+    gfcall = Expr(:call, getfield, sym, f)
+    if fieldcount(TF) ≡ 0
+      push!(t.args, gfcall)
+    elseif TF <: DataType
+      push!(t.args, :(StaticType{$gfcall}()))
+    else
+      newsym = gensym(sym)
+      push!(body.args, Expr(:(=), newsym, gfcall))
+      _append_fields!(t, body, newsym, TF)
+    end
+  end
+  return nothing
+end
+@generated function flatten_to_tuple(r::T) where {T}
+  body = Expr(:block, Expr(:meta,:inline))
+  t = Expr(:tuple)
+  if Base.issingletontype(T)
+    nothing
+  elseif fieldcount(T) ≡ 0
+    push!(t.args, :r)
+  elseif T <: DataType
+    push!(t.args, :(StaticType{r}()))
+  else
+    _append_fields!(t, body, :r, T)
+  end
+  push!(body.args, t)
+  body
+end
+function rebuild_fields(offset::Int, ::Type{T}) where {T}
+  call = (T <: Tuple) ? Expr(:tuple) : Expr(:new, T)
+  for f ∈ 1:fieldcount(T)
+    TF = fieldtype(T, f)
+    if Base.issingletontype(TF)
+      push!(call.args, TF.instance)
+    elseif fieldcount(TF) ≡ 0
+      push!(call.args, :($getfield(t, $(offset += 1), false)))
+    elseif TF <: DataType
+      push!(call.args, :($gettype($getfield(t, $(offset += 1), false))))
+    else
+      arg, offset = rebuild_fields(offset, TF)
+      push!(call.args, arg)      
+    end
+  end
+  return call, offset
+end
+@generated function reassemble_tuple(::Type{T}, t::Tuple) where {T}
+  if Base.issingletontype(T)
+    return T.instance
+  elseif fieldcount(T) ≡ 0
+    call = :($getfield(t, 1, false))
+  elseif T <: DataType
+    call = :($gettype($getfield(t, 1, false)))
+  else
+    call, _ = rebuild_fields(0, T)
+  end
+  Expr(:block, Expr(:meta,:inline), call)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using ArgDecomposition
 using Test
 
 @testset "ArgDecomposition.jl" begin
-    # Write your tests here.
+
+  for t ∈ (
+    Val(4), Float32, 8.9, (a = 3, b = (3,2), d = Float64, e = (1f0, '2', 3.0, 4, 0x05)),
+    (a = 3, b = (3,(2,21f0,3.0)), c = "hello, world!", d = Float64, e = (1f0, '2', 3.0, 4, 0x05))
+  )
+    
+    ft = flatten_to_tuple(t)
+    @test ft isa Tuple
+    rt = reassemble_tuple(typeof(t), ft)
+    @test rt === t
+  end  
 end
